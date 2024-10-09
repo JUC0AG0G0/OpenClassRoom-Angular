8a7d6a7a34c301273afed4e7c31fd2342e2b8566
Jules Corneille <j-corneille@logitud.local>
2024-10-09 11:15:42 +0200

    pagination + tri + recherche

--- src/app/face-snaps/components/face-snap-table/face-snap-table.component.ts
+++ src/app/face-snaps/components/face-snap-table/face-snap-table.component.ts
@@ -1,43 +1,99 @@
-import { Component, OnInit } from '@angular/core';
-import {MatTableModule} from '@angular/material/table';
-import { FaceSnap } from '../../../core/models/face-snap.model';
-import { FaceSnapsService } from '../../../core/services/face-snaps.service';
-import { Observable } from 'rxjs';
-import { CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray } from '@angular/cdk/drag-drop';
-import { DatePipe } from '@angular/common';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatSort, MatSortModule} from '@angular/material/sort';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+export interface UserData {
+  id: string;
+  name: string;
+  progress: string;
+  fruit: string;
+}
+
+const FRUITS: string[] = [
+  'blueberry',
+  'lychee',
+  'kiwi',
+  'mango',
+  'peach',
+  'lime',
+  'pomegranate',
+  'pineapple',
+];
+const NAMES: string[] = [
+  'Maia',
+  'Asher',
+  'Olivia',
+  'Atticus',
+  'Amelia',
+  'Jack',
+  'Charlotte',
+  'Theodore',
+  'Isla',
+  'Oliver',
+  'Isabella',
+  'Jasper',
+  'Cora',
+  'Levi',
+  'Violet',
+  'Arthur',
+  'Mia',
+  'Thomas',
+  'Elizabeth',
+];
 
 
-/**
- * @title Basic use of `<table mat-table>`
- */
 @Component({
-  selector: 'app-face-snap-table',
+  selector: 'table-overview-example',
+  styleUrl: 'face-snap-table.component.scss',
+  templateUrl: 'face-snap-table.component.html',
   standalone: true,
-  imports: [
-    MatTableModule, 
-    CdkDropList, 
-    CdkDrag, 
-    DatePipe
-  ],
-  templateUrl: './face-snap-table.component.html',
-  styleUrl: './face-snap-table.component.scss'
+  imports: [MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule],
 })
-export class FaceSnapTableComponent implements OnInit {
+export class FaceSnapTableComponent implements AfterViewInit {
+  displayedColumns: string[] = ['id', 'name', 'progress', 'fruit'];
+  dataSource: MatTableDataSource<UserData>;
 
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  @ViewChild(MatSort)
+  sort: MatSort = new MatSort;
 
-  faceSnaps$!: Observable<FaceSnap[]>;
+  constructor() {
+    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
 
-  constructor(private faceSnapsService: FaceSnapsService) { }
-
-
-  ngOnInit(): void {
-    this.faceSnaps$ = this.faceSnapsService.getAllFaceSnaps();;
-    
+    this.dataSource = new MatTableDataSource(users);
   }
 
-  drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.columns, event.previousIndex, event.currentIndex);
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
-  
-  columns: string[] = ['id', 'title', 'description', 'createdDate', 'snaps'];
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
+
+/** Builds and returns a new User. */
+function createNewUser(id: number): UserData {
+  const name =
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
+    ' ' +
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
+    '.';
+
+  return {
+    id: id.toString(),
+    name: name,
+    progress: Math.round(Math.random() * 100).toString(),
+    fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))],
+  };
 }
